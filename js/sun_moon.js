95ec57d809d8a0020c7d1ffdda64969c88ac65ca
Akihi11 <1821232988@qq.com>
2025-11-21 14:50:11 +0800

    Site updated: 2025-11-21 14:50:09

--- js/sun_moon.js
+++ js/sun_moon.js
@@ -1,7 +1,8 @@
 function switchNightMode() {
     document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="Cuteen_DarkSky"><div class="Cuteen_DarkPlanet"><div id="sun"></div><div id="moon"></div></div></div>'),
         setTimeout(function () {
-            document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')),
+            const modeicon = document.getElementById('modeicon');
+            document.querySelector('body').classList.contains('DarkMode') ? (document.querySelector('body').classList.remove('DarkMode'), localStorage.setItem('isDark', '0'), modeicon && modeicon.setAttribute('xlink:href', '#icon-moon')) : (document.querySelector('body').classList.add('DarkMode'), localStorage.setItem('isDark', '1'), modeicon && modeicon.setAttribute('xlink:href', '#icon-sun')),
                 setTimeout(function () {
                     document.getElementsByClassName('Cuteen_DarkSky')[0].style.transition = 'opacity 3s';
                     document.getElementsByClassName('Cuteen_DarkSky')[0].style.opacity = '0';
@@ -13,17 +14,20 @@ function switchNightMode() {
     const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
     if (nowMode === 'light') {
         // 先设置太阳月亮透明度
-        document.getElementById("sun").style.opacity = "1";
-        document.getElementById("moon").style.opacity = "0";
+        const sun = document.getElementById("sun");
+        const moon = document.getElementById("moon");
+        if (sun) sun.style.opacity = "1";
+        if (moon) moon.style.opacity = "0";
         setTimeout(function () {
-            document.getElementById("sun").style.opacity = "0";
-            document.getElementById("moon").style.opacity = "1";
+            if (sun) sun.style.opacity = "0";
+            if (moon) moon.style.opacity = "1";
         }, 1000);
 
         activateDarkMode()
         saveToLocal.set('theme', 'dark', 2)
         // GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
-        document.getElementById('modeicon').setAttribute('xlink:href', '#icon-sun')
+        const modeicon = document.getElementById('modeicon');
+        if (modeicon) modeicon.setAttribute('xlink:href', '#icon-sun')
         // 延时弹窗提醒
         // setTimeout(() => {
         //     new Vue({
@@ -42,16 +46,20 @@ function switchNightMode() {
         // }, 2000)
     } else {
         // 先设置太阳月亮透明度
-        document.getElementById("sun").style.opacity = "0";
-        document.getElementById("moon").style.opacity = "1";
+        const sun = document.getElementById("sun");
+        const moon = document.getElementById("moon");
+        if (sun) sun.style.opacity = "0";
+        if (moon) moon.style.opacity = "1";
         setTimeout(function () {
-            document.getElementById("sun").style.opacity = "1";
-            document.getElementById("moon").style.opacity = "0";
+            if (sun) sun.style.opacity = "1";
+            if (moon) moon.style.opacity = "0";
         }, 1000);
         
         activateLightMode()
         saveToLocal.set('theme', 'light', 2)
-        document.querySelector('body').classList.add('DarkMode'), document.getElementById('modeicon').setAttribute('xlink:href', '#icon-moon')
+        document.querySelector('body').classList.add('DarkMode');
+        const modeicon = document.getElementById('modeicon');
+        if (modeicon) modeicon.setAttribute('xlink:href', '#icon-moon')
         // setTimeout(() => {
         //     new Vue({
         //         data: function () {
